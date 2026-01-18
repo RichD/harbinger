1fdcd4ed36a57e402e7572172dad35f7be266ef1
Rich Dabrowski <engineering@richd.net>
2026-01-18 11:12:45 -0800

    Initial commit: harbinger gem scaffold
    Generated with bundle gem harbinger
    - RSpec for testing
    - RuboCop for linting
    - Changelog included

---
+++ spec/harbinger_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Harbinger do
+  it "has a version number" do
+    expect(Harbinger::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
