f5533985c08f585fb1889dcc72bc4da11d51078a
Rich Dabrowski <engineering@richd.net>
2026-01-18 22:45:48 -0800

    Require database detector modules in main library

--- lib/harbinger.rb
+++ lib/harbinger.rb
@@ -4,6 +4,9 @@ require_relative "harbinger/version"
 require_relative "harbinger/cli"
 require_relative "harbinger/analyzers/ruby_detector"
 require_relative "harbinger/analyzers/rails_analyzer"
+require_relative "harbinger/analyzers/database_detector"
+require_relative "harbinger/analyzers/postgres_detector"
+require_relative "harbinger/analyzers/mysql_detector"
 require_relative "harbinger/eol_fetcher"
 
 module Harbinger
